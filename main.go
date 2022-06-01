09cc75b53140e51c05926e678a3ec742eb6ebcf5
Ganesh Kale <ganesh@aiocdawacs.com>
2022-06-01 12:42:24 +0530

    new changes

--- main.go
+++ main.go
@@ -1,35 +1,25 @@
 package main
 
-import (
-	"github.com/kaleganeshrajan/middleware/app"
-	"github.com/kaleganeshrajan/middleware/controllers"
-	"github.com/kaleganeshrajan/middleware/service"
-	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-)
-
 var (
-	loginService    service.LoginService    = service.NewLoginService()
-	jwtService      service.JWTService      = service.NewJWTService()
+// loginService    service.LoginService    = service.NewLoginService()
+// jwtService      service.JWTService      = service.NewJWTService()
 
-	// loginController    controllers.LoginController    = controllers.NewLoginController(loginService, jwtService)
+// loginController    controllers.LoginController    = controllers.NewLoginController(loginService, jwtService)
 )
 
 func main() {
-	r := gin.Default()
-	gin.SetMode(gin.ReleaseMode)
+	// r := gin.Default()
+	// gin.SetMode(gin.ReleaseMode)
 
-	os.Setenv("TRUSTED_PROXY", "localhost")
-	r.SetTrustedProxies([]string{os.Getenv("TRUSTED_PROXY")})
+	// os.Setenv("TRUSTED_PROXY", "localhost")
+	// r.SetTrustedProxies([]string{os.Getenv("TRUSTED_PROXY")})
 
-	appObj := app.NewApp(r, controllers.Controller{})
+	// appObj := app.NewApp(r, controllers.Controller{})
 
-	appObj.MapUrls()
-	log.Printf("Url mapping done")
+	// appObj.MapUrls()
+	// log.Printf("Url mapping done")
 
-	log.Printf("Starting server")
-	r.Use(gin.Recovery())
-	r.Run("localhost:" + "5000")
+	// log.Printf("Starting server")
+	// r.Use(gin.Recovery())
+	// r.Run("localhost:" + "5000")
 }
--- models/return_obj.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type ReturnObj struct {
-	Success bool
-	Message string
-	Token   string
-}
