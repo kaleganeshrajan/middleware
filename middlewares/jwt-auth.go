11d6f071beeae5e5eb50fe0f16ec6c04d038b716
Ganesh Kale <ganesh@aiocdawacs.com>
2022-06-02 18:29:06 +0530

    remove unwanted code

--- middlewares/jwt-auth.go
+++ middlewares/jwt-auth.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// AuthorizeJWT validates the token from the http request, returning a 401 if it's not valid
+// AuthorizeJWT validates the token from cookies the http request, returning a 401 if it's not valid
 func AuthorizeJWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		logger.Info("Log validation started")
@@ -23,6 +23,38 @@ func AuthorizeJWT() gin.HandlerFunc {
 			logger.Error("Token string error",err)
 			c.AbortWithStatus(http.StatusInternalServerError)
 		}
+
+		token, err := service.NewJWTService().ValidateToken(tokenString, c)
+
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			 
+			logger.Info("Claims Details",
+				zap.Any("Claims[Name]: ", claims["name"]),
+				// zap.Any("Claims[Admin]: ", claims["admin"]),
+				zap.Any("Claims[Issuer]: ", claims["iss"]),
+				zap.Any("Claims[IssuedAt]: ", claims["iat"]),
+				zap.Any("Claims[ExpiresAt]: ", claims["exp"]))
+			
+		} else {
+			logger.Error("Token validation error",err)
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+	}
+}
+
+// AuthorizeJWT validates the token from authorization the http request, returning a 401 if it's not valid
+func AuthorizeJWTfromAuthorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		logger.Info("Log validation started")
+		const BEARER_SCHEMA = "Bearer "
+		authHeader := c.GetHeader("Authorization")
+		tokenString := authHeader[len(BEARER_SCHEMA):]
+		// tokenString, err := c.Cookie("token")
+		// if err != nil {
+		// 	logger.Error("Token string error",err)
+		// 	c.AbortWithStatus(http.StatusInternalServerError)
+		// }
 
 		token, err := service.NewJWTService().ValidateToken(tokenString, c)
 
