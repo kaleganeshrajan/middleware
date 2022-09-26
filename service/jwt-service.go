80da442b48715e90c5b98e8c82b375671e763442
Ganesh Kale <ganesh@aiocdawacs.com>
2022-09-26 14:59:23 +0530

    new changes

--- service/jwt-service.go
+++ service/jwt-service.go
@@ -89,11 +89,10 @@ func RefreshToken(tokenString string, session_time int64, c *gin.Context) error 
 	}
 	 
 	expirationTime := time.Now().Add(time.Minute * time.Duration(session_time))
-	fmt.Println(expirationTime)
-	fmt.Println(claims.ExpiresAt)
+	fmt.Println(time.Unix(claims.ExpiresAt, 0))	 
+	fmt.Println(claims.ExpiresAt, 0)	 
 	fmt.Println(time.Now().Unix())
 	claims.ExpiresAt = expirationTime.Unix()
-	fmt.Println(claims.ExpiresAt)
 	
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	t, err := token.SignedString([]byte(secrateKey))
